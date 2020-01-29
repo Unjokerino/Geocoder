95b2bd1323ee01b72aa2d8d45fa4fdac82a3d59c
Unjokerino <35525106+Unjokerino@users.noreply.github.com>
2020-01-29 16:55:38 +0500

    dn

--- js/index.js
+++ js/index.js
@@ -6,7 +6,7 @@ let current_adress_input = document.querySelector("#map_adress");
 let current_coords_input = document.querySelector("#map_coords");
 
 function init() {
-  var myPlacemark;
+  var placeMark;
   myMap = new ymaps.Map(
     "map",
     {
@@ -19,32 +19,27 @@ function init() {
   );
   current_adress_input.addEventListener("change", function(e) {
     if (e.target.value === "") {
-      console.log("gdsf");
-      myMap.geoObjects.remove(myPlacemark);
-      myPlacemark = false;
+      myMap.geoObjects.remove(placeMark);
+      placeMark = false;
     }
   });
-  // Слушаем клик на карте.
+
   myMap.events.add("click", function(e) {
     var coords = e.get("coords");
 
-    // Если метка уже создана – просто передвигаем ее.
-    if (myPlacemark) {
-      myPlacemark.geometry.setCoordinates(coords);
-    }
-    // Если нет – создаем.
-    else {
-      myPlacemark = createPlacemark(coords);
-      myMap.geoObjects.add(myPlacemark);
-      // Слушаем событие окончания перетаскивания на метке.
-      myPlacemark.events.add("dragend", function() {
-        getAddress(myPlacemark.geometry.getCoordinates());
+    if (placeMark) {
+      placeMark.geometry.setCoordinates(coords);
+    } else {
+      placeMark = createPlacemark(coords);
+      myMap.geoObjects.add(placeMark);
+
+      placeMark.events.add("dragend", function() {
+        getAddress(placeMark.geometry.getCoordinates());
       });
     }
     getAddress(coords);
   });
 
-  // Создание метки.
   function createPlacemark(coords) {
     return new ymaps.Placemark(
       coords,
@@ -63,34 +58,27 @@ function init() {
         results: 1
       })
       .then(function(res) {
-        // Выбираем первый результат геокодирования.
         var firstGeoObject = res.geoObjects.get(0),
-          // Координаты геообъекта.
           coords = firstGeoObject.geometry.getCoordinates(),
-          // Область видимости геообъекта.
           bounds = firstGeoObject.properties.get("boundedBy");
 
-        if (myPlacemark) {
-          myPlacemark.geometry.setCoordinates(coords);
-        }
-        // Если нет – создаем.
-        else {
-          myPlacemark = createPlacemark(coords);
-          myMap.geoObjects.add(myPlacemark);
-          // Слушаем событие окончания перетаскивания на метке.
-          myPlacemark.events.add("dragend", function() {
-            getAddress(myPlacemark.geometry.getCoordinates());
+        if (placeMark) {
+          placeMark.geometry.setCoordinates(coords);
+        } else {
+          placeMark = createPlacemark(coords);
+          myMap.geoObjects.add(placeMark);
+
+          placeMark.events.add("dragend", function() {
+            getAddress(placeMark.geometry.getCoordinates());
           });
         }
-        // Масштабируем карту на область видимости геообъекта.
+
         myMap.setBounds(bounds, {
-          // Проверяем наличие тайлов на данном масштабе.
           checkZoomRange: true
         });
       });
   });
 
-  // Определяем адрес по координатам (обратное геокодирование).
   function getAddress(coords) {
     ymaps.geocode(coords).then(function(res) {
       var firstGeoObject = res.geoObjects.get(0);
